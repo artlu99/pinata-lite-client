5b5df579baf1ff4b6d9cf33249f0ced94cea677d
artlu99 <alu99.sm@gmail.com>
2024-05-09 14:08:59 -0400

    logo

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import siteMeta from "@/config/site.config";
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start gap-12 mt-12">
-      <Image src={siteMeta.logo} alt="logo" className="" width={350} height={350} />
+      <Image src={siteMeta.ogImage} alt="logo" className="" width={350} height={350} />
       <Auth />
       <Separator className="sm:w-[500px] w-sm" />
       <Feed />
