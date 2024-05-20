dda69cedda7d31bbd2f50f01306e4ed2f3e93bec
artlu99 <alu99.sm@gmail.com>
2024-05-19 21:35:05 -0400

    move workers endpoint

--- components/bookmarks.tsx
+++ components/bookmarks.tsx
@@ -6,7 +6,7 @@ import { FarcasterEmbed } from "react-farcaster-embed/dist/client";
 import "react-farcaster-embed/dist/styles.css";
 
 const fid = 6546;
-const endpoint = "https://worker-misty-voice-905f.artlu.workers.dev/?fid=";
+const endpoint = "https://decent-bookmarks.artlu.workers.dev/?fid=";
 
 const NUM_BOOKMARKS_SHOWN = 3;
 const BOOKMARKS_ORDER: "ASC" | "DESC" = "DESC";
