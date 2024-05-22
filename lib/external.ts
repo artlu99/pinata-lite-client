0c2b0e0e407077bec7d2f36902eb9efc5a1f56c7
artlu99 <alu99.sm@gmail.com>
2024-05-21 22:22:01 -0400

    Revert "revalidate: 0"
    This reverts commit 38a292f6c7f2ab10c0e2eb7ad4924160a29f72e3.

--- lib/external.ts
+++ lib/external.ts
@@ -19,16 +19,14 @@ export interface BookmarksResponse {
 export async function fetchFCAN(): Promise<FCANResponse> {
   const endpoint =
     "https://fcan.xyz/getadsfor?fid=391262&src=client-bcbhshow.artlu.xyz";
-  const res = await fetch(`${endpoint}`, { next: { revalidate: 0 } });
+  const res = await fetch(`${endpoint}`);
   if (!res.ok) throw new Error("Failed to fetch data");
   return res.json();
 }
 
-export async function fetchDecentBookmarks(
-  fid: number
-): Promise<BookmarksResponse> {
+export async function fetchDecentBookmarks(fid: number): Promise<BookmarksResponse> {
   const endpoint = "https://decent-bookmarks.artlu.xyz/?fid=";
-  const res = await fetch(endpoint + fid, { next: { revalidate: 0 } });
+  const res = await fetch(endpoint + fid);
   if (!res.ok) throw new Error("Failed to fetch data");
   return res.json();
 }
